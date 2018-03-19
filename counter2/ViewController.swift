4b69f9bf509fb9a4c1ed2afb5d4bed67f6283019
akzmffls <d7703_14@d7703-14ui-imac.local>
2018-03-19 15:28:20 +0900

    counter

--- counter2/ViewController.swift
+++ counter2/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lt: UILabel!
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func bt(_ sender: Any) {
+        counter = counter + 1
+        print("#(bt(button pressed")
+        lt.text = String(counter)
+    }
+    
+    @IBAction func bt2(_ sender: Any) {
+        counter = 0
+        lt.text = String(counter)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
